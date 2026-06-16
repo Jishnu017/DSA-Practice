19ea87b5c17e127259e978932b9a7574a880cf61
Jishnu017 <163671182+Jishnu017@users.noreply.github.com>
2026-06-16 08:20:36 -0400

    Time: 28 ms (88%), Space: 48 MB (44%) - LeetHub

---
+++ 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.java
@@ -0,0 +1,46 @@
+class Solution {
+    public char processStr(String s, long k) {
+        long len=0;
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            char c=s.charAt(i);
+            switch(c){
+                case '*':
+                    if(len>0){
+                        len--;
+                    }
+                    break;
+                case '#':
+                    len=len*2;
+                case '%':
+                    break;
+                default:
+                    len++;
+                    break;
+            }
+        }
+        if(k+1>len) return '.';
+        for(int i=n-1;i>=0;i--){
+            char c=s.charAt(i);
+            switch(c){
+                case '*':
+                    len++;
+                    break;
+                case '#':
+                    if(k+1>(len+1)/2){
+                        k-=len/2;
+                    }
+                    len=(len+1)/2;
+                    break;
+                case '%':
+                    k=len-k-1;
+                    break;
+                default:
+                   if(k+1==len) return c;
+                   len--;
+                    break;
+            }
+        }
+        return '.';
+    }
+}
