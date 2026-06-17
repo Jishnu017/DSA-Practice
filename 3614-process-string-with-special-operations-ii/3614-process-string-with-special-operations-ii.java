a25d73e61868ac34b69818e06ef5df235c08bd1c
Jishnu017 <163671182+Jishnu017@users.noreply.github.com>
2026-06-17 12:48:22 -0400

    Time: 30 ms (42.31%), Space: 48 MB (26.92%) - LeetHub

--- 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.java
+++ 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.java
@@ -4,25 +4,23 @@ class Solution {
         int n=s.length();
         for(int i=0;i<n;i++){
             char c=s.charAt(i);
-            switch(c){
+            switch (c){
                 case '*':
-                    if(len>0){
-                        len--;
-                    }
+                    if(len>0) len--;
                     break;
                 case '#':
-                    len=len*2;
+                    len*=2;
+                    break;
                 case '%':
                     break;
                 default:
                     len++;
-                    break;
-            }
+        }
         }
         if(k+1>len) return '.';
         for(int i=n-1;i>=0;i--){
             char c=s.charAt(i);
-            switch(c){
+            switch (c){
                 case '*':
                     len++;
                     break;
@@ -36,10 +34,10 @@ class Solution {
                     k=len-k-1;
                     break;
                 default:
-                   if(k+1==len) return c;
-                   len--;
+                    if(k+1==len) return c;
+                    len--;
                     break;
-            }
+        }
         }
         return '.';
     }
