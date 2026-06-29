ce14487e1060e706eff21d7ed08c7c2ef8278a7c
Jishnu017 <163671182+Jishnu017@users.noreply.github.com>
2026-06-29 10:28:28 +0530

    Time: 5 ms (8.23%), Space: 46.1 MB (10.4%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+
+    public List<List<Integer>> combinationSum(int[] arr, int target) {
+        Arrays.sort(arr);
+        bt(0, 0, arr, new ArrayList<>(), target);
+        return ans;
+    }
+
+    void bt(int sum, int i, int[] arr, List<Integer> l, int t) {
+        if (sum == t) {
+            ans.add(new ArrayList<>(l));
+            return;
+        }
+        if (sum > t) {
+            return;
+        }
+        for(int j=i;j<arr.length;j++){
+            l.add(arr[j]);
+            sum = sum + arr[j];
+            bt(sum, j, arr, l, t);
+            sum = sum - arr[j];
+            l.remove(l.size() - 1);
+        }
+    }
+}
