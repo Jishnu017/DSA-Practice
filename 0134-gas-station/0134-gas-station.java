429ab60b01a032eff06c1ffa9b666cc4ef44e8ea
Jishnu017 <163671182+Jishnu017@users.noreply.github.com>
2026-07-29 21:26:24 +0530

    Time: 2 ms (99.24%), Space: 119.3 MB (65.23%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -6,13 +6,13 @@ class Solution {
             tot+=gas[i]-cost[i];
         }
         if(tot<0) return -1;
-        int currG=0,in=0;
+        int curr=0,in=0;
         for(int i=0;i<n;i++){
-           currG+=gas[i]-cost[i];
-           if(currG<0){
-            in=i+1;
-            currG=0;
-           }
+            curr+=gas[i]-cost[i];
+            if(curr<0){
+                curr=0;
+                in=i+1;
+            }
         }
         return in;
     }
